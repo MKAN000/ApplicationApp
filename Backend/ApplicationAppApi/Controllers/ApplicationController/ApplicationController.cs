bf3aa727993f00377d1e3e8bfb91aa9f0f2dbdcc
Mateusz Nosel <jakubnosel2e@interia.pl>
2024-03-27 11:23:48 +0100

    Applicant Model Create

---
+++ Backend/ApplicationAppApi/Controllers/ApplicationController/ApplicationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ApplicationAppApi.Controllers.ApplicationController
+{
+    public class ApplicationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Backend/ApplicationAppApi/Models/ApplicantModel/ApplicantModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApplicationAppApi.Models.ApplicantModel
+{
+    public class ApplicantModel
+    {
+        [Key]
+        public int AlbumNumber { get; set; }
+        public string Rank { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Subdivision { get; set; }
+        public string FacultyGroup { get; set; }
+    }
+}
