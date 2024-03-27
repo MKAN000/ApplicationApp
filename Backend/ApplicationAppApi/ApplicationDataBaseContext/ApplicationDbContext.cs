39b14db846e7e4cc68085eda6189dfa01f0c2804
Mateusz Nosel <jakubnosel2e@interia.pl>
2024-03-27 11:42:24 +0100

    Initializing connection with DB

---
+++ Backend/ApplicationAppApi/ApplicationDataBaseContext/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using ApplicationAppApi.Models.ApplicantModel;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApplicationAppApi.ApplicationDataBaseContext
+{
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options) { }
+        
+        public DbSet<ApplicantModel> Applicants { get; set; }
+                
+        
+    }
+}
--- Backend/ApplicationAppApi/Program.cs
+++ Backend/ApplicationAppApi/Program.cs
@@ -1,11 +1,45 @@
-var builder = WebApplication.CreateBuilder(args);
+using ApplicationAppApi.ApplicationDataBaseContext;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
+var builder = WebApplication.CreateBuilder(args);
+var configuration = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json")
+    .Build();
 // Add services to the container.
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    var connectionString = configuration.GetConnectionString("applicationDbContext");
+    options.UseSqlServer(connectionString);
+}, ServiceLifetime.Scoped);
+
+builder.Services.AddIdentity<IdentityUser,  IdentityRole>(options =>
+{
+    options.Password.RequireDigit = true;
+    options.Password.RequiredLength = 8;
+    options.Password.RequireNonAlphanumeric = true;
+    options.Password.RequireUppercase = true;
+    options.Password.RequireLowercase = true;
+})
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultTokenProviders();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAnyOrigin", builder =>
+    {
+        builder
+            .AllowAnyOrigin()
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
 
 var app = builder.Build();
 
@@ -18,8 +52,12 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
+
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.UseCors("AllowAnyOrigin");
 
 app.Run();
